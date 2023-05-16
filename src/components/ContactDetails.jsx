4213db17bc935d2b68a375c9e9e8552ffb3cd304
Alvin strandberg <Str4ndb3rg@live.se>
2023-05-16 14:49:16 +0200

    fixed error with links

--- src/components/ContactDetails.jsx
+++ src/components/ContactDetails.jsx
@@ -3,13 +3,16 @@ import "../css/AboutMe/contactdetails.css";
 function ContactDetails() {
   return (
     <div className="contact-container">
-      <a href="https://github.com/troskan">
+      <a target="_blank" href="https://github.com/troskan">
         <img src="src/assets/GitHub_Logo_White.png" alt="" />
       </a>
-      <a href="https://www.linkedin.com/in/alvin-strandberg-612954253/">
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/alvin-strandberg-612954253/"
+      >
         <img src="src/assets/linkedin.png" alt="" />
       </a>
-      <a href="mailto: alvin.strandberg@proton.me">
+      <a target="_blank" href="mailto: alvin.strandberg@proton.me">
         <img src="src/assets/Graphicloads-100-Flat-2-Email.256.png" alt="" />
       </a>
     </div>
