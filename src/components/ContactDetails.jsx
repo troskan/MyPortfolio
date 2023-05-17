c7e7ead148a6c4305ed55037c41ba087bd3e4e10
Alvin strandberg <Str4ndb3rg@live.se>
2023-05-17 11:11:12 +0200

    corrected image src

--- src/components/ContactDetails.jsx
+++ src/components/ContactDetails.jsx
@@ -1,19 +1,22 @@
 import "../css/AboutMe/contactdetails.css";
+import GithubPic from "./assets/GitHub_Logo_White.png";
+import LinkedIn from "./assets/linkedin.png";
+import Email from "./assets/Graphicloads-100-Flat-2-Email.256.png";
 
 function ContactDetails() {
   return (
     <div className="contact-container">
       <a target="_blank" href="https://github.com/troskan">
-        <img src="src/assets/GitHub_Logo_White.png" alt="" />
+        <img src={GithubPic} alt="" />
       </a>
       <a
         target="_blank"
         href="https://www.linkedin.com/in/alvin-strandberg-612954253/"
       >
-        <img src="src/assets/linkedin.png" alt="" />
+        <img src={LinkedIn} alt="" />
       </a>
       <a target="_blank" href="mailto: alvin.strandberg@proton.me">
-        <img src="src/assets/Graphicloads-100-Flat-2-Email.256.png" alt="" />
+        <img src={Email} alt="" />
       </a>
     </div>
   );
