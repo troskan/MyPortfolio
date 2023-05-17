65c446ab70716e4f1ba3c96d2e70f0f4ff7a2d17
Alvin strandberg <Str4ndb3rg@live.se>
2023-05-17 21:40:37 +0200

    added a status report in blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,6 +4,14 @@ function Blog() {
   return (
     <div>
       <h1>Blog</h1>
+      <h2 className="wip">
+        ⚠️ This blog is a work in progress.
+        <br />
+        Follow progress here:{" "}
+        <a href="https://github.com/troskan/BlogManagementAPI" target="_blank">
+          Github
+        </a>{" "}
+      </h2>
 
       <BlogItems />
     </div>
