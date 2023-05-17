bda15f33469183dbdb2355dfbd8db440326bb23d
Alvin strandberg <Str4ndb3rg@live.se>
2023-05-17 10:54:17 +0200

    added blog again

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+import BlogItems from "../components/BlogItems";
+
+function Blog() {
+  return (
+    <div>
+      <h1>Blog</h1>
+
+      <BlogItems />
+    </div>
+  );
+}
+
+export default Blog;
