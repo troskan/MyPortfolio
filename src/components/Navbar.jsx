36a808373c4640980cb3f0dfba0ca964e89bfcff
Alvin strandberg <Str4ndb3rg@live.se>
2023-06-19 15:19:50 +0200

    removed /portfolio route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,14 @@ import "../css/Navbar/nav.css";
 function Navbar() {
   return (
     <div className="nav-container">
+      {/* <h1 className="alvin">Alvin Strandberg</h1> */}
       <nav>
         <NavLink to="/" activeclassname="active">
           About Me
         </NavLink>
-        <NavLink to="/portfolio" activeclassname="active">
+        {/* <NavLink to="/portfolio" activeclassname="active">
           Portfolio
-        </NavLink>
+        </NavLink> */}
         <NavLink to="/blog" activeclassname="active">
           Blog
         </NavLink>
