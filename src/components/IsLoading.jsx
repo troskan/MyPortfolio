f4f1ff203e2a5db45d9bd7d2f7118c6d2fbf1957
troskan <str4ndb3rg@live.se>
2023-06-28 23:32:23 +0200

    fixed margins on home page.

--- src/components/IsLoading.jsx
+++ src/components/IsLoading.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Stack from "@mui/material/Stack";
 import LinearProgress from "@mui/material/LinearProgress";
+import "../css/Blog/blog.css";
 
 function IsLoading(props) {
   // Corrected prop name
   return (
     <div className="center">
       <Stack sx={{ width: "30%", color: "grey.500" }} spacing={1}>
-        <h2>Loading {props.string}..</h2>
+        <h2 id="stack-header">Loading {props.string}..</h2>
         <LinearProgress color="success" />
       </Stack>
     </div>
