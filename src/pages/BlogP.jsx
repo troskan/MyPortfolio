9f7025553edef69b45aeb4a778a8e3dd0e0ca7d5
Alvin strandberg <Str4ndb3rg@live.se>
2023-12-07 21:23:55 +0100

    test

--- src/pages/BlogP.jsx
+++ src/pages/BlogP.jsx
@@ -1,12 +1,9 @@
 import BlogPosts from "../components/blog/BlogPosts";
-<<<<<<< HEAD
-// import FilterPosts from "../components/blog/FilterPosts";
-=======
+
 import FilterPosts from "../components/blog/FilterPosts";
 import "../css/Blog/blog_structure.css";
 import BlogLinks from "../components/blog/BlogLinks";
 import { Filter } from "@mui/icons-material";
->>>>>>> a9391107faef95a0545c9fb52aa72960de8fccfa
 
 function BlogP() {
   return (
