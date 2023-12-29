82dc092cf2cb62b802e9b1ef4d3dffd916fbf0bd
Alvin strandberg <Str4ndb3rg@live.se>
2023-12-30 00:47:24 +0100

    new link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,24 @@ import "../css/Navbar/nav.css";
 
 function Navbar() {
   return (
-    <div className="nav-container">
-      {/* <h1 className="alvin">Alvin Strandberg</h1> */}
-      <nav>
-        <NavLink to="/" activeclassname="active">
-          About Me
-        </NavLink>
-        {/* <NavLink to="/portfolio" activeclassname="active">
+    <>
+      <div className="nav-container">
+        {/* <h1 className="alvin">Alvin Strandberg</h1> */}
+        <nav>
+          <NavLink to="/" activeclassname="active">
+            About Me
+          </NavLink>
+          {/* <NavLink to="/portfolio" activeclassname="active">
           Portfolio
         </NavLink> */}
-        <NavLink to="/blog" activeclassname="active">
-          Blog
-        </NavLink>
-      </nav>
-    </div>
+          <NavLink to="/blog" activeclassname="active">
+            Blog
+          </NavLink>
+        </nav>
+      </div>
+
+      <a href="https://blog-react-ts.vercel.app/">Try the new blog (Alpha)</a>
+    </>
   );
 }
 
